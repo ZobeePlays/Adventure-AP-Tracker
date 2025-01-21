5b4e49941eb5561838bc9fd67355d3a1874555e0
ZobeePlays <159688147+ZobeePlays@users.noreply.github.com>
2025-01-21 03:09:37 -0600

    Add files via upload

---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,17 @@
+ITEM_MAPPING = {
+	[118000011] = {"yellowkey", "toggle"},
+	[118000012] = {"whitekey", "toggle"},
+	[118000013] = {"blackkey", "toggle"},
+	[118000010] = {"bridge", "toggle"},
+	[118000017] = {"magnet", "toggle"},
+	[118000009] = {"sword", "toggle"},
+	[118000016] = {"chalice", "toggle"},
+	[118000256] = {"leftdifficultyswitch", "toggle"},
+	[118000257] = {"rightdifficultyswitch", "toggle"},
+	[118000258] = {"freeincarnate", "consumable"},
+	[118000259] = {"slowyorgle", "toggle"},
+	[118000260] = {"slowgrundle", "toggle"},
+	[118000261] = {"slowrhindle", "toggle"},
+	[118000262] = {"revivedragons", "toggle"},
+	[118000000] = {"nothing", "toggle"},
+}
